c459e4ba49f02a2e1e570e9ea0a80b357f0c1f93
Danielle Heberling <danielle.heberling@navabenefits.com>
2023-02-12 20:15:21 -0800

    Refactor

--- index.js
+++ index.js
@@ -2,29 +2,27 @@ import { load } from 'cheerio'
 const page = process.env.page || 1
 
 // Fetch everything from the Hacker News homepage
-const hnpage = await fetch(`https://news.ycombinator.com/?p=${page}`)
-const data = await hnpage.text()
+const hackerNewPage = await fetch(`https://news.ycombinator.com/?p=${page}`)
+const data = await hackerNewPage.text()
 
 const $ = load(data)
 const pageContent = []
 
 $('.athing').each((_index, el) => {
-  const item = $(el)
-  const rank = item.find('.title > .rank').text()
-  const title = item.find('.title').find('a').first().text()
-  const link = item.find('.titleline a').attr('href')
-  const subline = item.next()
+  const article = $(el)
+  const subline = article.next()
+
+  const rank = article.find('.title > .rank').text()
   const author = subline.find('.hnuser').text().trim()
   const points = subline.find('.score').text()
-  const age = subline.find('.age').text()
   const comments = subline.find('a').last().text()
 
   pageContent.push({
     rank: rank ? Number(rank.replace('.', '')) : null,
-    title,
-    link,
-    author: author.trim().length ? author : null,
-    age,
+    title: article.find('.title').find('a').first().text(),
+    link: article.find('.titleline a').attr('href'),
+    author: author.length ? author : null,
+    age: subline.find('.age').text(),
     points: points ? Number(points.replace(' points', '')) : null,
     comments: comments.includes('comments')
       ? Number(comments.replace('comments', '').trim())
