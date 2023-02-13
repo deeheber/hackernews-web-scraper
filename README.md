a86f6509152ec90f9b916395b2335f2df7f7f889
Danielle Heberling <danielle.heberling@navabenefits.com>
2023-02-12 17:52:05 -0800

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # hackernews-web-scraper
-A simple node.js webscraper to retrieve the homepage of hackernews
+
+A lightweight node.js webscraper to retrieve the homepage of hackernews
+
+## System Requirements
+
+This repo utilizes the experimental `fetch` in node and top level `await`. To run this repo, you will need to use node 18 or higher (see `.nvmrc` for the exact version this was designed for).
+
+## Instructions To Run
+
+1. Clone the repo
+2. `npm i`
+3. `npm start`
+
+It'll `console.log` the results of the scripe.
+
+## Response Body
+
+It'll be an array of objects, each object being a post on the homepage of hackernews. The object will have the following properties:
+
+```
+{
+  rank,
+  title,
+  link,
+  author,
+  age,
+  points,
+  comments
+}
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+import { load } from 'cheerio'
+
+// Fetch everything from the Hacker News homepage
+const page = await fetch('https://news.ycombinator.com/')
+const data = await page.text()
+
+const $ = load(data)
+const pageContent = []
+
+$('.athing').each((_index, el) => {
+  const item = $(el)
+  const rank = item.find('.title > .rank').text()
+  const title = item.find('.title').find('a').first().text()
+  const link = item.find('.titleline a').attr('href')
+  const subline = item.next()
+  const author = subline.find('.hnuser').text().trim()
+  const points = subline.find('.score').text()
+  const age = subline.find('.age').text()
+  const comments = subline.find('a').last().text()
+
+  pageContent.push({
+    rank: rank ? Number(rank.replace('.', '')) : null,
+    title,
+    link,
+    author: author.trim().length ? author : null,
+    age,
+    points: points ? Number(points.replace(' points', '')) : null,
+    comments: comments.includes('comments')
+      ? Number(comments.replace('comments', '').trim())
+      : null,
+  })
+})
+
+console.log(pageContent)
